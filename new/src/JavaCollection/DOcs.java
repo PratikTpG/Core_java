dede2c80d573cd6876ecc76ca9c05d19f50379bc
ASHISH AVIRAL <ashishaviral@ASHISHs-MacBook-Pro.local>
2024-06-10 16:18:54 +0545

    pratik

---
+++ new/src/JavaCollection/DOcs.java
@@ -0,0 +1,34 @@
+package JavaCollection;
+
+public class DOcs {
+    /*
+    •⁠  ⁠*collectiondocs.java X
+
+        •ListTest.java D Book.java
+        •booktest.java E Settestjava D MapTest.java
+        public class collectiondocs €
+        java collection/ collection framework---
+        #used to store and processing dynamic multiple data/objects in java.
+        1> list: dynamic array
+        can be implement in two ways-
+        ie Array list ie
+        Linked list
+        : Slow processing, better to use for data storage : Fast processing, better to use for data processing
+        2> set: collection of unique data/objects.
+        ie Hashset
+        : Unique data only
+        ie
+        Treeset
+        : Unique + Sorted data
+        ie
+        linkedhashset
+        : Unique + Insertion in data
+        3> Map : data manage in key value pair. easy
+        ie HashMap
+        : <k, v> pair
+        ie TreeMap
+        : <k, v> + Sorted data
+        ie LinkedHashMap
+        : <k, v› + Insertion in data
+     */
+}
---
+++ new/src/JavaCollection/MapTest.java
@@ -0,0 +1,24 @@
+package JavaCollection;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class MapTest {
+    public static void main(String[] args) {
+//                Map<String, Integer> map = new HashMap<>();
+//
+//                map.put("english" ,90);
+//                map.put("NEapli",40);
+//                map.put("Chemistry", 20);
+//                map.put("Physics",50);
+//
+//                for (String key : map.keySet()){
+//                    System.out.println(key+"=" +map.get(key));
+//                }
+
+//                Map<String , <Integer> > mlist = new LinkedHashMap<>();
+//
+//    }
+
+}
